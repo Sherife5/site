9414d0fea1778441a43eb0cf909ff7a8f5f99acd
Sherife5 <94749139+Sherife5@users.noreply.github.com>
2021-11-20 16:55:45 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Brygada+1918&family=Lora&family=Roboto:ital@0;1&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/adaptive.css">
+    <title>PobTech 1</title>
+</head>
+<body>
+    <header class="header">
+        <div class="header__main">
+            <div class="container">
+                <div class="header__inner">
+                    <div class="header__inner-logo">
+                        <a class="header__inner-title" href="#">PobTech 1</a>
+                    </div>
+                    <nav class="header__menu">
+                        <div class="btn__menu">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <ul class="header__menu-list">
+                            <li>
+                                <a class="header__menu-link" href="#">
+                                    О нас
+                                </a>
+                            </li>
+                            <li>
+                                <a class="header__menu-link" href="#">
+                                    Товары
+                                </a>
+                            </li>
+                            <li>
+                                <a class="header__menu-link" href="#">
+                                    Помошь
+                                </a>
+                            </li>
+                            <li>
+                                <a class="header__menu-link" href="#">
+                                    Контакты
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="header__info">
+                <div class="header__inner-info">
+                    <h1 class="header__inner-title">
+                        Втсупление
+                    </h1>
+                    <p class="header__inner-text">
+                        Наша бытовая техника надежна, стабильна, универсальна и удобна в использовании. Производится на лучших фабриках и фабриках страны!
+                    </p>
+                    <div class="header__inner-btn">
+                    <a  href="#">
+                        Посмотреть
+                    </a>
+                </div>
+                </div>
+                <div class="header__inner-photo">
+                    <img src="Lab3.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<section class="product">
+    <div class="container">
+        <h3 class="product__title">
+            Наши товары
+        </h3>
+        <p class="product__name">
+            Пылесосы
+        </p>
+        <div class="slider">
+            <div class="slider__item">
+                <img class="slider__item-img" src="Vacum.jpg" alt="">
+                <p class="slider__item-info">
+                    Пылесос без мешка Philips 2000 series XB2125/09
+                    <span>цена: 2799грн</span>
+                </p>
+                <a class="slider__item-link" href="#">
+                    Читать дальше
+                </a>
+            </div>
+        </div>
+        <p class="product__name">
+            Холодильники
+        </p>
+        <div class="slider">
+            <div class="slider__item">
+                <img class="slider__item-img" src="Fridge.jpg" alt="">
+                <p class="slider__item-info">
+                    Двухкамерный холодильник INDESIT XIT8 T2E X
+                    <span>цена: 12599грн</span>
+                </p>
+                <a class="slider__item-link" href="#">
+                    Читать дальше
+                </a>
+            </div>
+        </div>
+        <p class="product__name">
+                Электрочайники
+        </p>
+        <div class="slider">
+            <div class="slider__item">
+                <img class="slider__item-img" src="Electric kettle.jpg" alt="">
+                <p class="slider__item-info">
+                    Электрочайник RZTK KS 2217E Led
+                    <span>цена: 595грн</span>
+                </p>
+                <a class="slider__item-link" href="#">
+                    Читать дальше
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
